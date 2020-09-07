2390272e926093fecc9dbaa9a5d2188a2c99f353
ezequiel <ezequielm@gmail.com>
2020-09-07 06:39:30 -0400

    moved generic icons to LDTGaffer, removed resources folder

--- gaffer-examples.sh
+++ gaffer-examples.sh
@@ -1,10 +1,11 @@
 # Gaffer-examples
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export GAFFER_EXAMPLES=$SCRIPT_DIR
-export GAFFERUI_IMAGE_PATHS=$GAFFERUI_IMAGE_PATHS:$GAFFER_EXAMPLES
+export GAFFERUI_IMAGE_PATHS=$GAFFERUI_IMAGE_PATHS:$GAFFER_EXAMPLES/assets/icons
 export GAFFER_REFERENCE_PATHS=$GAFFER_REFERENCE_PATHS:$GAFFER_EXAMPLES
-source $SCRIPT_DIR/LDTGaffer/LDTGaffer.sh
 exe() { echo "\$ ${@/eval/}" ; "$@" ; }
 
 # ECHO GAFFER PATHS FOR INSPECTION
 exe eval "export |grep GAFFER"
+
+source $SCRIPT_DIR/LDTGaffer/LDTGaffer.sh

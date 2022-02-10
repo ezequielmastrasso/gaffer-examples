b11f01dbea2a3176ab20c532ea5447dbdbebf27e
eze <ezequielm@gmail.com>
2022-02-10 07:20:01 -0500

    Readme and renders updated
    gfr files updates
    
    
    new wall textures 
    
    
    new barbershop files
    
    
    ignore mb files
    
    
    fastforward LDTGafer
    
    
    6k dwaa for github size limits

--- expand-assets.sh
+++ expand-assets.sh
@@ -1,4 +1,3 @@
 GAFFER_EXAMPLES="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-file-roller -h $GAFFER_EXAMPLES/assets/barbershop/abc/babershop_interior.abc.tar.7z
-file-roller -h $GAFFER_EXAMPLES/assets/barbershop/abc/babershop_interior.abc.tar
+file-roller -h $GAFFER_EXAMPLES/assets/barbershop/abc/babershop_interior.abc.7z
